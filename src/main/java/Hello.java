fb2489c848676e5b56709143224637973c4c7d19
Caglar Keskin <caglarkeskin@gmail.com>
2020-09-30 20:13:47 +0200

    Create .gitignore

---
+++ src/main/java/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("This is just for to understand Git and Github");
+    }
+
+}
---
+++ src/test/java/HelloTest.java
@@ -0,0 +1,7 @@
+public class HelloTest {
+
+    public static void main(String[] args) {
+        System.out.println("This is just for to understand Git and Github");
+        System.out.println("Lets check the changes that we made");
+    }
+}
