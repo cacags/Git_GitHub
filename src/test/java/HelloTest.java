17f556dd6482bd89138bc53fb8ebcfd74df84325
Caglar Keskin <caglarkeskin@gmail.com>
2020-09-30 21:07:23 +0200

    I am just trying

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -4,5 +4,6 @@ public class HelloTest {
         System.out.println("This is just for to understand Git and Github");
         System.out.println("Lets check the changes that we made");
         System.out.println("Lets check the changes that we made again");
+        System.out.println("Hello again");
     }
 }
