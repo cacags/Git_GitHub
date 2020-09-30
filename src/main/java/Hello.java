f0b64541f657a639e1ef73d333d8be0a8e29b66b
Caglar Keskin <caglarkeskin@gmail.com>
2020-09-30 21:09:47 +0200

    todays first change

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,6 +2,7 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("This is just for to understand Git and Github");
+        System.out.println("I am just trying to understand what is going on here");
     }
 
 }
